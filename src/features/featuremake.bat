585b359f309cc33de38b2aebfc2c50791b47ba35
cat3582 <cstbwy@gmail.com>
2022-06-05 00:00:44 +0900

    first

---
+++ src/features/featuremake.bat
@@ -0,0 +1,10 @@
+md,feature\api
+md,feature\assets
+md,feature\components
+md,feature\hooks
+md,feature\routes
+md,feature\store
+md,feature\types
+type nul >feature\types\index.ts
+md,feature\utils
+type nul >feature\index.ts
